81ce15a960792ab568a5a37da95426b503a78a02
Johnjk031 <john.juhlinkarlsson@gmail.com>
2021-08-17 10:28:52 +0200

    Init

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 8080;
+
+app.get('/', (req, res ) => {
+    res.send('hello world')
+})
+
+app.get('/api/numbers', (req, res) => {
+    res.send(['alfa', 'beta', 'gamma'])
+})
+
+app.listen(port, () => console.log(`http://localhost:${port}/`))
