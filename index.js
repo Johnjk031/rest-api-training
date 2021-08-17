01d7bc0f67f1fef3b29327003a8fbee4411f34da
Johnjk031 <john.juhlinkarlsson@gmail.com>
2021-08-17 11:28:07 +0200

    added letters

--- index.js
+++ index.js
@@ -2,12 +2,27 @@ const express = require('express')
 const app = express()
 const port = 8080;
 
+const alphabet = [
+    {id: 1, name: 'Alpha'},
+    {id: 2, name: 'Beta'},
+    {id: 3, name: 'Gamma'}
+
+]
+
 app.get('/', (req, res ) => {
     res.send('hello world')
 })
 
-app.get('/api/numbers', (req, res) => {
-    res.send(['alfa', 'beta', 'gamma', 'delta'])
+app.get('/api/alphabet', (req, res) => {
+    res.send(alphabet)
 })
+
+
+app.get('/api/alphabet/:id', (req, res) => {
+   const alpha = alphabet.find(a => a.id === parseInt(req.params.id));
+   if (!alpha) res.status(404).send('Letter not found')
+   res.send(alpha)
+})
+
 
 app.listen(port, () => console.log(`http://localhost:${port}/`))
